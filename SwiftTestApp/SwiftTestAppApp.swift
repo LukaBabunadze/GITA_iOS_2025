fc0efe39471aadc6c9ec40f75a0474a780304f77
LukaBabunadze <luka.babunadze.1@iliauni.edu.ge>
2025-02-19 22:00:48 +0400

    Initial Commit

---
+++ SwiftTestApp/SwiftTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestAppApp.swift
+//  SwiftTestApp
+//
+//  Created by Luka Babunadze on 19.02.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
